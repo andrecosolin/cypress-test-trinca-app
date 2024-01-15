abfe74693ac92a609712f946eb8ce3f30a8b0529
Djonatas Borges <interdjonatas@gmail.com>
2024-01-14 22:32:10 -0300

    Inserindo Descrição e modo de uso de Commands.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,10 @@
+/**
+ * @param {Object} inputData - Objeto que representa os dados para registrar um usuário.
+ *   Serão levados em consideração as seguintes keys: email e password
+ * 
+ * Exemplo de uso:
+ *  cy.gui_register({email: 'userTest@gmail.com', password: 'test'})
+ */
 Cypress.Commands.add('gui_register', (inputData={}) => {
   cy.visit('/cadastro')
   if (inputData.email !== undefined) {
@@ -25,6 +32,12 @@ Cypress.Commands.add('gui_register', (inputData={}) => {
   cy.get('button[type="submit"]').click()
 })
 
+/**
+ *   Comando para realizar login e manter a sessão do mesmo. Ele não utiliza parâmetros, pois
+ *   usa os dados capturados das ENVs.
+ * Exemplo de uso:
+ *   cy.gui_loginWithSession()
+ */
 Cypress.Commands.add('gui_loginWithSession', () => {
   const userEmail = Cypress.env('EMAIL')
   const userPassword = Cypress.env('PASSWORD')
@@ -52,7 +65,13 @@ Cypress.Commands.add('gui_loginWithSession', () => {
   cy.session(userEmail, login, options)
 })
 
-
+/**
+ * @param {Object} inputData - Objeto que representa os dados de login do usuário.
+ *   Serão levados em consideração as seguintes keys: email e password
+ * 
+ * Exemplo de uso:
+ *  cy.gui_login({email: 'userTest@gmail.com', password: 'test'})
+ */
 Cypress.Commands.add('gui_login', (inputData={}) => {
   console.log(inputData)
   cy.visit('/acesso')
@@ -79,6 +98,19 @@ Cypress.Commands.add('gui_login', (inputData={}) => {
   cy.contains('button', 'Acessar conta').click()
 })
 
+/**
+ * @param {Object} eventFormData - Objeto que representa o formulário de eventos.
+ *   Serão levados em consideração as seguintes keys: title{String}, price{Number}, date{Date} e observations{String}
+ * 
+ * Exemplo de uso:
+ *  const eventToCreateEvent = {
+      title: faker.lorem.sentence(4),
+      price: 50,
+      date: '2024-07-08',
+      observations: faker.lorem.lines(1),
+    }
+ * cy.gui_fillEventForm(eventToCreateEvent)
+ */
 Cypress.Commands.add('gui_fillEventForm', (eventFormData={}) => {
   if (eventFormData.title !== undefined) {
     cy.get('input[name="title"]').then((el) => {
@@ -125,12 +157,39 @@ Cypress.Commands.add('gui_fillEventForm', (eventFormData={}) => {
   }
 })
 
+/**
+ * @param {Object} eventFormData - Objeto que representa o formulário de eventos.
+ *   Serão levados em consideração as seguintes keys: title{String}, price{Number}, date{Date} e observations{String}
+ *
+ * Exemplo de uso:
+ *  const eventToCreateEvent = {
+      title: faker.lorem.sentence(4),
+      price: 50,
+      date: '2024-07-08',
+      observations: faker.lorem.lines(1),
+    }
+ * cy.gui_createEvent(eventToCreateEvent)
+ */
 Cypress.Commands.add('gui_createEvent', (eventFormData={}) => {
   cy.get('button:contains("Cadastrar novo evento")').click()
   cy.gui_fillEventForm(eventFormData)
   cy.get('button:contains("Salvar evento")').click()
 })
 
+/**
+ * @param {String} eventName - Nome do evento a ser editado
+ * @param {Object} eventFormData - Objeto que representa o formulário de eventos.
+ *   Serão levados em consideração as seguintes keys: title{String}, price{Number}, date{Date} e observations{String}
+ *
+ * Exemplo de uso:
+ *  const eventToCreateEvent = {
+      title: faker.lorem.sentence(4),
+      price: 50,
+      date: '2024-07-08',
+      observations: faker.lorem.lines(1),
+    }
+ * cy.gui_createEvent(eventToCreateEvent)
+ */
 Cypress.Commands.add('gui_editEvent', (eventName, eventFormData={}) => {
   cy.contains('strong', eventName).siblings('a').click()
   cy.gui_fillEventForm(eventFormData)
